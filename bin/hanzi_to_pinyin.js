68a129a4149029a5ed9c81929d146af5648849b2
wxianfeng <wang.fl1429@gmail.com>
2013-08-26 18:49:32 +0800

    hanzi_to_pinyin bin

--- bin/hanzi_to_pinyin.js
+++ bin/hanzi_to_pinyin.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
 
 var hanzi_to_pinyin = require('../lib/hanzi_to_pinyin');
-console.log(hanzi_to_pinyin.hanzi_to_pinyin());
+var args = process.argv.slice(2);
+console.log(hanzi_to_pinyin.hanzi_to_pinyin(args[0]));
